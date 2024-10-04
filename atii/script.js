38b1b6ac2f15613869e68dca8c63fc05739a9b9c
Lucas Nascimento <lucasthuba@gmail.com>
2024-10-04 17:29:52 -0300

    aula de js

---
+++ atii/script.js
@@ -0,0 +1,14 @@
+function mostrarConteudo1() {
+    const conteudo = document.getElementById("conteudo");
+    conteudo.innerHTML = "<p>Este é o conteúdo do botão 1.</p>";
+}
+
+function mostrarConteudo2() {
+    const conteudo = document.getElementById("conteudo");
+    conteudo.innerHTML = "<p>Este é o conteúdo do botão 2.</p>";
+}
+
+function mostrarConteudo3() {
+    const conteudo = document.getElementById("conteudo");
+    conteudo.innerHTML = "<p>Este é o conteúdo do botão 3.</p>";
+}
---
+++ exe.js
@@ -0,0 +1,10 @@
+function modelo(){
+    let elementP = document.getElementById("paragrafo");
+    elementP.style.backgroundColor = "lightblue";
+
+    let elementA = document.getElementById("link");
+    elementA.href = "https://pixeldots.com";
+
+    let elemenPg = document.getElementById("res");
+    elemenPg.innerText = "texto";
+}
